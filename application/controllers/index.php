c6c66274f3cbd8d5249d8be09d87bf5c005e4a57
Huang <chikinwong214@users.noreply.github.com>
2020-02-22 11:52:24 +0800

    20200222
    测试phpstorm的fetch和pull功能

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -1,7 +1,7 @@
 <?php
 class Index extends CI_Controller{
 //    与类名相同，会被php当作构造方法
-//相当于__construct(){}
+//相当于__construct(){} 但php7已经不再支持这种代码
     public function index(){
     echo 'index控制器的index方法<br>';
 }
